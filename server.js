4acf7e9c5a670ae888a670c2509fe81a609b1253
Alexandre LEGAL <@mail>
2021-10-25 13:40:36 +0200

    crapping crunchy + wakanim

--- server.js
+++ server.js
@@ -24,17 +24,27 @@ const puppeteer = require('puppeteer');
 // });
 
 (async () => {
-    const browser = await puppeteer.launch({headless: false});
+    const browser = await puppeteer.launch({headless: true});
     const page = await browser.newPage();
-
-
+    //setUserAgent permet que le code fonctionne partout meme si headless est a true 
+    //(Why this is interesting? Suppose you want to scrap a website using Puppeteer in headless mode and the target website put a protection by detecting the User Agent string (blocking ChromeHeadless) then your scraping activity might be blocked.)
+    await page.setUserAgent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.75 Safari/537.36")
     // await page.goto('https://www.wakanim.tv/fr/v2/whats-new');
     // const wakanim = await page.evaluate(() => {
     //     let adn = []
     //     let elements = document.querySelectorAll('div.WhatsNew-container:nth-child(4) > section:nth-child(1) > div:nth-child(2) > ul:nth-child(1) > li');
     //     for (element of elements){
     //         adn.push({
-    //             titre: element.querySelector('.slider_item_title > strong').innerHTML
+    //             title: element.querySelector('.slider_item_title > strong')?.innerHTML,
+    //             img: element.querySelector('.slider_item_image > img')?.getAttribute('data-src'),
+    //             season: element.querySelector('.slider_item_season_title')?.innerText.trim(),
+    //             episodeSortie: element.querySelector('.tooltip_text > strong')?.innerHTML,
+    //             synopsis: element.querySelector('.tooltip_text > br')?.nextSibling.data.trim(),
+    //             genres: Array.from(element.querySelectorAll('.tooltip_genre')).map(e => e.text.replace(', ','')),
+    //             originalName: element.getAttribute('data-originalname'),
+    //             rating: element?.getAttribute('data-rating'),
+    //             subbed: element?.getAttribute('data-subbed'),
+    //             dubbed: element?.getAttribute('data-dubbed'),
     //         })
     //     }
     //     return adn;
@@ -42,19 +52,46 @@ const puppeteer = require('puppeteer');
     // console.log(wakanim);
 
 
-    await page.goto('https://www.crunchyroll.com/lineup');
+    await page.goto('https://www.crunchyroll.com/fr/videos/anime/seasons/fall-2021');
     // await page.waitForNavigation();
-    const crunchyroll = await page.evaluate(() => {
-        let adn = []
-        // console.log(window);
-        let elements = document.querySelectorAll('div.lineup-grid:nth-child(4) > ul:nth-child(1) > li');
-        for (element of elements){
-            adn.push({
-                titre: element.querySelector('.lineup-series-title').innerHTML
-            })
-        }
-        return adn;
+    const crunchyrollAnimes = await page.evaluate(() => {
+        return Array.from(document.querySelectorAll('.portrait-grid > li')).map(e => 'http://www.crunchyroll.com/' + e.querySelector('.portrait-element').getAttribute('href'))
     });
-    console.log(crunchyroll);
+console.log(crunchyrollAnimes)
+let crunch= [];
+    for(element of crunchyrollAnimes){
+        await page.goto(element + '/more');
+        const crunchyroll = await page.evaluate(() => {
+        return {
+            titre: document.querySelector('h1.ellipsis > span')?.innerHTML,
+            img: document.querySelector('.poster')?.getAttribute('src'),
+            synopsis: document.querySelector('.series-extended-information')?.innerText,
+            rating: document.querySelector('div.xsmall-margin-bottom:nth-child(2) > div:nth-child(2) > meta')?.getAttribute('content'),
+            sortis: document.querySelector('.large-margin-bottom:nth-child(3) > .strong')?.textContent.replace('Simulcast en ligne ', '').replace('les ', ''),
+            genres : document.querySelectorAll('.large-margin-bottom:nth-child(5) li:nth-child(2) > a'),
+        }
+        });
+        crunch.push(crunchyroll);
+        console.log(crunchyroll)
+    }
+    console.log(crunch);
+    // const crunchyroll = await page.evaluate(() => {
+    //     let adn = []
+    //     let elements = document.querySelectorAll('.portrait-grid > li');
+    //     for (element of elements){
+    //         adn.push({
+    //             titre: element.querySelector('.series-title').innerHTML,
+    //             nbEpisode: element.querySelector('.series-data').innerHTML.trim(),
+	// 			image: element.querySelector('.portrait').getAttribute('src'),
+    //         })
+    //     }
+    //     return adn;
+    // });
+    // console.log(crunchyroll);
+
+    // await page.goto('https://gw.api.animedigitalnetwork.fr/show/simulcast?year=2021');
+    // // TODO API ADN
+    // console.log(adn);
+    
     await browser.close();
 }) ();
