6ca8fb727969efddd58f4349d8e17c81549351c4
Alexandre LEGAL <503p2.alexandre.legal@gmail.com>
2021-12-31 21:49:02 +0100

    info about

---
+++ client/src/components/About.js
@@ -0,0 +1,12 @@
+const About = () => {
+    return (  
+        <div className="about">
+            <h2>A propos de moi</h2>
+            <p>Adresse mail : 503p2.alexandre.legal@gmail.com</p>
+            <a href="https://github.com/AlexandreLEGAL"> Mon Github : AlexandreLEGAL</a><br></br>
+            <a href="https://www.linkedin.com/in/alexandre-legal/"> Mon Linkedin : Alexandre LEGAL</a>
+        </div>
+    );
+}
+ 
+export default About;
